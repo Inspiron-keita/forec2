50477577fdb44e9da0154b0e59f9d5d85b18e4d7
Inspiron-keita <1ono.13kita98@gmail.com>
2020-07-05 17:00:50 +0900

    test selenium

--- test.py
+++ test.py
@@ -1,4 +1,21 @@
-x=1
-y=1
-print("test Calculation",x+y)
-#tesuto
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+def _main():
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless')
+    driver = webdriver.Chrome(options=options)
+
+    driver.get('https://www.google.co.jp/')
+    search = driver.find_element_by_name('q')
+    search.send_keys('Python')
+    search.send_keys(Keys.RETURN)
+
+    time.sleep(3)
+    driver.save_screenshot('search_results.png')
+
+    driver.quit()
+
+if __name__ == '__main__':
+    _main()
